a519286ce53a73215484425d67926c9eb469872a
Vikaash Harichandran <jbvikaash@gmail.com>
2021-12-22 18:38:57 +0800

    Update linked-list.js

--- javascript/data-structure/linked-list.js
+++ javascript/data-structure/linked-list.js
@@ -11,13 +11,35 @@ class LinkedList {
         this.length = 0
     }
 
+    returnSelf() {
+        return this
+    }    
+
     incrementLength() {
         this.length++
     }
 
+    decrementLength() {
+        this.length--
+    }
+
+    throwError(e) {
+        console.error("Invalid operation: ", e)
+    }
+
+    join() {
+        let str = ""
+
+        for(let node of this.head) {
+            str += node.data + ", "
+        }
+
+        return str
+    }
+
     prepend(value) {
         this.head.unshift(new Node(value))
-        this.length++
+        this.incrementLength()
 
         if(this.head[1]) {
             this.head[0].next = this.head[1]
@@ -28,7 +50,7 @@ class LinkedList {
 
     append(value) {
         this.head.push(new Node(value))
-        this.length++
+        this.incrementLength()
 
         if(this.head[1]) {
             this.head[this.length - 2].next = this.head[length - 1]
@@ -38,6 +60,33 @@ class LinkedList {
     }
 
     insertAfter(index, value) {
-        
+        if(index == (this.length - 1)) {
+            this.append(value)
+        } else if((index >= this.length) || (index < 0)) {
+            this.throwError("Reference index for insertAfter() does not exist!")
+        } else {
+            this.head.splice(index, 0, new Node(value))
+            this.incrementLength()
+        }
+    }
+
+    insertBefore(index, value) {
+        if(index == 0) {
+            this.prepend(value)
+        } else if((index >= this.length) || (index <= 0)) {
+            this.throwError("Reference index for insertAfter() does not exist!")
+        } else {
+            this.head.splice(index, 0, new Node(value))
+            this.incrementLength()
+        }
     }    
 }
+
+let test_linkedList = new LinkedList()
+
+test_linkedList.append(3)
+test_linkedList.prepend(6)
+test_linkedList.insertAfter(0, 5)
+test_linkedList.insertBefore(1, 3)
+
+console.log(test_linkedList.join())
